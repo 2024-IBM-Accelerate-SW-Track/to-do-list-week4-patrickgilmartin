505824ad4e35304a05c1566b8217ee3d047d0fcc
patrickgilmartin <144620815+patrickgilmartin@users.noreply.github.com>
2024-07-08 22:28:06 -0400

    Update AddTodo.js

--- src/component/AddTodo.js
+++ src/component/AddTodo.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import { Button, TextField } from "@mui/material";
-<<<<<<< HEAD
 import { DesktopDatePicker, LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
 import { format } from 'date-fns';
@@ -8,18 +7,10 @@ import Axios from "axios";
 
 
 class AddTodo extends Component {
-=======
-import { DesktopDatePicker , LocalizationProvider} from '@mui/x-date-pickers';
-import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
-
-class AddTodo extends Component {
-  // Create a local react state of the this component with both content date property set to nothing.
->>>>>>> 6c174a370c08c86a2581337c2cea2fc9edbe9de3
   constructor() {
     super();
     this.state = {
       content: "",
-<<<<<<< HEAD
       due: null, 
     };
   }
@@ -72,61 +63,11 @@ class AddTodo extends Component {
 
   render() {
     return (
-=======
-      date: "",
-      duedate: null
-    };
-  }
-  // The handleChange function updates the react state with the new input value provided from the user and the current date/time.
-  // "event" is the defined action a user takes. In this case, the event is triggered when the user types something
-  // into the text field.
-  handleChange = (event) => {
-    this.setState({
-      content: event.target.value,
-      date: Date()
-    });
-  };
-
-  handleDateChange = (event) => {
-    let date = null
-    if(event != null){
-      date = new Date(event)
-    }
-    this.setState({
-      duedate: date
-    });
-  };
-  // The handleSubmit function collects the forms input and puts it into the react state.
-  // event.preventDefault() is called to prevents default event behavior like refreshing the browser.
-  // this.props.addTodo(this.state) passes the current state (or user input and current date/time) into the addTodo function defined
-  // in the Home.js file which then adds the input into the list.
-  handleSubmit = (event) => {
-    event.preventDefault();
-    if (this.state.content.trim()) {
-      this.props.addTodo(this.state);
-      this.setState({
-        content: "",
-        date: "",
-        duedate: null
-      });
-    }
-  };
-  render() {
-    return (
-      // 1. When rendering a component, you can render as many elements as you like as long as it is wrapped inside
-      // one div element.
-      // 2. The return statement should include a text field input with the handleChange function from above that
-      // is passed into an onChange event.
-      // 3. The return should also include a button with the handleSubmit function from above that is passed into
-      // an OnClick event.
-      // 4. The value of the text field also should reflect the local state of this component.
->>>>>>> 6c174a370c08c86a2581337c2cea2fc9edbe9de3
       <div>
         <TextField
           label="Add New Item"
           variant="outlined"
           onChange={this.handleChange}
-<<<<<<< HEAD
           value={this.state.content} 
           inputProps={{ "data-testid": "new-item-textfield" }}      
         />
@@ -137,17 +78,6 @@ class AddTodo extends Component {
             value={this.state.due ? new Date(this.state.due) : null}
             onChange={this.handleDateChange}
             renderInput={(params) => <TextField {...params} />}
-=======
-          value={this.state.content}
-        />
-        <LocalizationProvider dateAdapter={AdapterDateFns}>         
-          <DesktopDatePicker
-              id="new-item-date"
-              label="Due Date"
-              value={this.state.duedate}
-              onChange={this.handleDateChange}
-              renderInput={(params) => <TextField {...params} />}
->>>>>>> 6c174a370c08c86a2581337c2cea2fc9edbe9de3
           />
         </LocalizationProvider>
         <Button
@@ -155,10 +85,7 @@ class AddTodo extends Component {
           onClick={this.handleSubmit}
           variant="contained"
           color="primary"
-<<<<<<< HEAD
           data-testid="new-item-button"
-=======
->>>>>>> 6c174a370c08c86a2581337c2cea2fc9edbe9de3
         >
           Add
         </Button>
